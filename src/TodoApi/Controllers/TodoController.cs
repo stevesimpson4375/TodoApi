95a77f25f74315b98de5601a048a57e9b5fdfe28
steve simpson <stevesimpson4375@gmail.com>
2017-01-28 21:54:15 -0500

    Created initial API

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item); 
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key}, item);
+        }
+    }
+}
--- src/TodoApi/Controllers/ValuesController.cs
+++ src/TodoApi/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     public class ValuesController : Controller
     {
         // GET api/values
